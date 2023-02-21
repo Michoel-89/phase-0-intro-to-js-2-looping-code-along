b62edb4c1a5fd6cd2c9fd294d406577e1485018d
Michoel Rivkin <michoelr111@gmail.com>
2023-02-21 16:37:10 -0500

    commit tree 18c8d6fde223d4edf7a5ec88a3f3d3e70f8b8872

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+
+}
+
+const gifts = ['teddy bear', 'drone', 'doll' ]
+function wrapGifts(gifts){
+for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    
+}
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+
+function writeCards(names, string){
+  let array = []
+  for (let i = 0; i < names.length; i++) { 
+     array.push(`Thank you, ${names[i]}, for the wonderful ${string} gift!`);
+    }
+debugger
+  return array;
+  
+     }
+     console.log(writeCards(['Eddie', 'John', 'Mike'], 'birthday'));
+  function countDown(countDown = 10) { 
+    while (countDown >= 0)  
+    console.log(countDown--)
+    
+    }
+    countDown(10);
+  
+  
