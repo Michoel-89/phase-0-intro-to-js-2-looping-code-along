40ebac76faf3832f912130cbb07d776c6d70c86d
michoel <michoelr111@gmail.com>
2023-05-02 11:47:13 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+function writeCards(names, day) {
+    let makeCard = [];
+    for (let i = 0; i < names.length; i++) {
+        makeCard.push( `Thank you, ${names[i]}, for the wonderful ${day} gift!`);
+    }
+    return makeCard;
+}
+function countDown(num) {
+    let i = 0
+    while (i <= num) {
+        console.log(num);
+        num--;
+    }
+}
